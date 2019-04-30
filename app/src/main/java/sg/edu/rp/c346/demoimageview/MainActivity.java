706de0adfbbb3de127fd00c304bedbcc12468615
17010247 <17010247@myrp.edu.sg>
2019-04-30 10:34:26 +0800

    Message commit

--- app/src/main/java/sg/edu/rp/c346/demoimageview/MainActivity.java
+++ app/src/main/java/sg/edu/rp/c346/demoimageview/MainActivity.java
@@ -6,15 +6,26 @@ import android.widget.ImageView;
 
 public class MainActivity extends AppCompatActivity {
 
-    ImageView ivDay2;
+    ImageView ivDay1, ivDay2, ivDay3, ivDay4, ivDay5;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        ivDay2 = findViewById(R.id.imageView2);
+        ivDay1 = findViewById(R.id.imageView);
+        ivDay1.setImageResource(R.drawable.day1);
 
+        ivDay2 = findViewById(R.id.imageView2);
         ivDay2.setImageResource(R.drawable.day2);
+
+        ivDay3 = findViewById(R.id.imageView3);
+        ivDay3.setImageResource(R.drawable.day3);
+
+        ivDay4 = findViewById(R.id.imageView4);
+        ivDay4.setImageResource(R.drawable.day4);
+
+        ivDay5 = findViewById(R.id.imageView5);
+        ivDay5.setImageResource(R.drawable.day5);
     }
 }
